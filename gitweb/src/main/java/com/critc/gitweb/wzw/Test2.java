0367ea8ec109b71ae3f374bd2b187421ea131aa6
wutongbuyu <2369953414@qq.com>
2018-08-14 15:21:53 +0800

    Change-Id: Iea6baaa502e4ac4baa7b3e7de9fb5415601f4feb Signed-off-by: wutongbuyu <2369953414@qq.com>

---
+++ gitweb/src/main/java/com/critc/gitweb/wzw/Test2.java
@@ -0,0 +1,23 @@
+package com.critc.gitweb.wzw;
+
+public class Test2 {
+	private String d;
+	private String s;
+	@Override
+	public String toString() {
+		return "Test2 [d=" + d + ", s=" + s + "]";
+	}
+	public String getD() {
+		return d;
+	}
+	public void setD(String d) {
+		this.d = d;
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+	
+}
